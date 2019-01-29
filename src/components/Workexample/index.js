a93050d7b37052964cd8dbb5e5b3b3b66211ab53
charlotteflygare <38113960+charlotteflygare@users.noreply.github.com>
2019-01-29 20:33:59 +0100

    Style

--- src/components/Workexample/index.js
+++ src/components/Workexample/index.js
@@ -10,9 +10,20 @@ class Workexample extends React.Component {
     return (
       <div className="workexample-container-workexample">
 
+
+      <div className="workexample">
+        <div className="workexample-image">
+         <Link to="/projectpage/1"><img src="./images/somn/somn_thumb.jpg" alt="workexample comprend thumbnail"/></Link>
+        </div>
+        <div className="workexample-text">
+          <h4>Min doktor</h4>
+          <p>Campaign page</p>
+        </div>
+       </div>
+
         <div className="workexample">
           <div className="workexample-image1">
-           <Link to="/projectpage/1"><img src="./images/comprend/comprend_thumb.jpg" alt="workexample comprend thumbnail"/></Link>
+           <Link to="/projectpage/2"><img src="./images/comprend/comprend_thumb.jpg" alt="workexample comprend thumbnail"/></Link>
           </div>
           <div className="workexample-text1">
             <h4>Comprend</h4>
@@ -24,7 +35,7 @@ class Workexample extends React.Component {
          <div className="workexample-text2">
          </div>
          <div className="workexample-image2">
-           <Link to="/projectpage/2"><img src="./images/techspressen/techspressen_thumb.jpg" alt="workexample techspressen thumbnail"/></Link>
+           <Link to="/projectpage/3"><img src="./images/techspressen/techspressen_thumb.jpg" alt="workexample techspressen thumbnail"/></Link>
             <h4>Techspressen</h4>
             <p>A remake of the newspaper Expressen</p>
           </div>
@@ -34,7 +45,7 @@ class Workexample extends React.Component {
            <div className="workexample-text4">
            </div>
            <div className="workexample-image4">
-              <Link to="/projectpage/3"><img src="./images/form/form_thumb.jpg" alt="workexample form thumbnail"/></Link>
+              <Link to="/projectpage/4"><img src="./images/form/form_thumb.jpg" alt="workexample form thumbnail"/></Link>
               <h4>Form</h4>
               <p>Create an account in a form</p>
             </div>
